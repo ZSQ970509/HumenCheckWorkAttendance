698111360008078d585a33c99922b8ad59fb9945
Your Name <you@example.com>
2017-11-30 14:16:07 +0800

    更新

--- app/src/main/java/com/example/humencheckworkattendance/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/humencheckworkattendance/ui/activity/LoginActivity.java
@@ -69,6 +69,8 @@ public class LoginActivity extends BaseActivity<LoginPresenter> implements Login
     private final int SDK_PERMISSION_REQUEST = 127;
     private String permissionInfo;
     AlertDialog dialog = null;
+
+    //11111111111111111111111111111111111111111111111111111111111111111111111
     Handler myHandler = new Handler() {
         public void handleMessage(Message msg) {
             switch (msg.what) {
